caa7e2fce010629e5e10249932a8ec010d911261
vishakha <vishakha.tyagi@olx.com>
2021-06-15 14:36:25 +0530

    cleaning the gallery listeners when not needed

--- mediapicker/src/main/java/com/mediapicker/gallery/Gallery.kt
+++ mediapicker/src/main/java/com/mediapicker/gallery/Gallery.kt
@@ -7,20 +7,26 @@ import com.mediapicker.gallery.presentation.carousalview.CarousalActionListener
 object Gallery{
     internal lateinit var galleryConfig : GalleryConfig
 
-    internal var pagerCommunicator: GalleryPagerCommunicator ?= null
+    internal var pagerCommunicator: GalleryPagerCommunicator?= null
 
-    internal var carousalActionListener: CarousalActionListener ?= null
+    internal var carousalActionListener: CarousalActionListener? = null
 
-    fun init(galleryConfig: GalleryConfig){
+    fun init(galleryConfig: GalleryConfig) {
         this.galleryConfig = galleryConfig
     }
 
-    fun updateCommunicator(galleryCommunicator: IGalleryCommunicator){
+    fun updateCommunicator(galleryCommunicator: IGalleryCommunicator?) {
         galleryConfig.galleryCommunicator = galleryCommunicator
     }
 
     internal fun getApp() = galleryConfig.applicationContext
 
     internal fun getClientAuthority() = galleryConfig.clientAuthority
+
+    fun clean() {
+        pagerCommunicator = null
+        carousalActionListener = null
+        galleryConfig.galleryCommunicator = null
+    }
 
 }
